c1c32a9e8abbf5b05e85c27ae532fa91b8c00f49
L-Gabri <64975343+L-Gabri@users.noreply.github.com>
2024-09-07 21:03:08 -0300

    atualizando dados

--- dados.js
+++ dados.js
@@ -2,26 +2,57 @@ let dados = [
 {
     titulo : "Real Madrid",
     descricao : "O Real Madrid é, sem sombra de dúvidas, o clube mais vitorioso da história da UEFA Champions League (15 taças). Com um histórico de conquistas que se estende por décadas, o time merengue construiu um legado imbatível na principal competição de clubes da Europa. A equipe madridista coleciona títulos como troféus, demonstrando uma consistência impressionante e uma capacidade única de superar desafios em finais emocionantes. A paixão dos torcedores e a tradição do clube fazem com que cada conquista na Champions League seja celebrada como um marco no futebol mundial, consolidando o Real Madrid como o verdadeiro rei da Europa.",
-    link : "https://www.realmadrid.com/pt-PT",
-    instagram: "https://www.instagram.com/realmadrid/"
+    link : "https://www.realmadrid.com",
+    instagram : "https://www.instagram.com/realmadrid/",
+    tags : "maior espanha espanhol"
 },
 {
     titulo : "Milan",
     descricao : "O Milan AC é um dos clubes mais tradicionais e vitoriosos do futebol italiano e europeu contendo 7 títulos da maior competição de clubes da Europa. Sua história na UEFA Champions League é marcada por uma elegância e um estilo de jogo que o tornaram um dos maiores representantes do futebol europeu. Com vários torcedores apaixonados e muita tradição, o Rossonero conquistou diversos títulos da competição, demonstrando uma capacidade única de se adaptar às diferentes épocas do futebol. A história do Milan na Champions League é repleta de momentos memoráveis, jogadores lendários e finais inexplicáveis, consolidando o clube como um dos grandes protagonistas da UEFA Champions League.",
-    link : "https://www.acmilan.com/pt-PT",
-    instagram: "https://www.instagram.com/acmilan/"
+    link : "https://www.acmilan.com",
+    instagram : "https://www.instagram.com/acmilan/",
+    tags : "italia itália milano"
 },
 {
     titulo : "Liverpool",
     descricao : "O Liverpool Football Club é um dos clubes mais icônicos e apaixonantes do futebol mundial. Sua história na UEFA Champions League é marcada por momentos épicos tendo ao todo 6 títulos conquistados, finais emocionantes e uma legião de torcedores que vibram a cada conquista. Com um futebol intenso e apaixonado, os Reds conquistaram o coração de milhões ao redor do globo. A equipe de Anfield Road possui uma rica tradição na competição, com títulos históricos que marcaram gerações. A capacidade de superação e a garra dos jogadores do Liverpool transformaram o clube em um verdadeiro sinônimo de sucesso na Champions League, deixando um legado duradouro na história do futebol.",
-    link : "https://www.liverpoolfc.com/PT",
-    instagram: "https://www.liverpoolfc.com/PT"
+    link : "https://www.liverpoolfc.com",
+    instagram : "https://www.instagram.com/liverpoolfc/",
+    tags: "ingles inglês inglaterra"
 },
 {
     titulo : "Bayern",
     descricao : "O Bayern de Munique é um dos gigantes do futebol europeu e um dos clubes mais vitoriosos da história da UEFA Champions League tendo ao todo 6 títulos da competição. Com uma tradição de excelência e um futebol eficiente, o time bávaro demonstra uma consistência impressionante ao longo das décadas. A equipe alemã é conhecida por sua formação de jogadores talentosos, sua organização tática e sua capacidade de dominar os jogos. A história do Bayern na Champions League é marcada por momentos históricos e goleadas espetaculares, consolidando o clube como uma das forças dominantes do futebol europeu. A paixão dos torcedores bávaros e a tradição do clube fazem com que cada conquista na Champions League seja celebrada como um marco na história do futebol alemão.",
-    link : "https://www.fcbayern.com/PT",
-    instagram: "https://www.fcbayern.com/PT"
+    link : "https://www.fcbayern.com",
+    instagram : "https://www.instagram.com/fcbayern/",
+    tags: "bavaro alemao alemão bayer"
 },
+{
+    titulo : "Barcelona",
+    descricao : "O Barcelona é um dos clubes mais tradicionais e vitoriosos da história da Champions League (5 taças). Com um estilo de jogo característico, marcado pela posse de bola e a busca por um futebol bonito, o time catalão conquistou a admiração de milhões de fãs ao redor do mundo. Ao longo dos anos, o Barça acumulou títulos e protagonizou partidas memoráveis, consolidando seu lugar entre as maiores equipes do continente. A combinação de craques, um sistema de base forte e uma filosofia de jogo única fazem do Barcelona um dos favoritos eternos na principal competição de clubes da Europa.",
+    link : "https://www.fcbarcelona.com",
+    instagram : "https://www.instagram.com/fcbarcelona/",
+    tags: "cataluña catalunha barca espanha espanhol"
+},
+{
+    titulo : "Ajax",
+    descricao : "O Ajax, gigante do futebol holandês, possui uma rica história na UEFA Champions League. O clube de Amsterdã já conquistou o título máximo do futebol europeu em quatro oportunidades, sendo um dos poucos clubes a vencer a competição três vezes consecutivas. Com um estilo de jogo ofensivo e técnico, o Ajax revelou grandes talentos para o futebol mundial e protagonizou diversas partidas memoráveis na Champions League. A equipe holandesa é sinônimo de tradição e paixão, e continua a ser uma força a ser considerada em cada edição da competição.",
+    link : "https://www.ajax.nl",
+    instagram : "https://www.instagram.com/afcajax/",
+    tags: "holanda holandes"
+},
+{
+    titulo : "Manchester United",
+    descricao : "O Manchester United é um dos clubes mais tradicionais e vitoriosos da história da UEFA Champions League (3 títulos). Com uma torcida apaixonada e um histórico de grandes jogadores, os Red Devils conquistaram o coração dos fãs de futebol ao redor do mundo. Ao longo dos anos, o clube inglês acumulou títulos e protagonizou partidas memoráveis, consolidando seu lugar entre as maiores equipes do continente. A combinação de tradição, paixão e um elenco de qualidade fazem do Manchester United um dos favoritos eternos na principal competição de clubes da Europa.",
+    link : "https://www.manutd.com",
+    instagram : "https://www.instagram.com/manchesterunited/",
+    tags: "ingles inglês inglaterra utd"
+},
+{
+    titulo : "Internazionale Milano",
+    descricao : "A Internazionale de Milão, também conhecida como Inter de Milão, é um dos clubes mais tradicionais e vitoriosos do futebol italiano, com uma rica história na UEFA Champions League. A equipe nerazzurra já conquistou o título máximo do futebol europeu em três oportunidades, sendo um dos clubes mais bem-sucedidos da Itália na competição. Com um estilo de jogo pragmático e eficiente, a Inter de Milão revelou grandes talentos para o futebol mundial e protagonizou diversas partidas memoráveis na Champions League. A equipe italiana é sinônimo de paixão e tradição, e continua a ser uma força a ser considerada em cada edição da competição.",
+    link : "https://www.inter.it",
+    instagram : "https://www.instagram.com/inter/",
+    tags: "italia itália"
+}
 ];
-    
